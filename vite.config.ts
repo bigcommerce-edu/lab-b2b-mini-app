e000094bff90df709548e1412c2e92afbff9d330
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-11-04 10:01:23 -0600

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: './src/main.tsx',
+    },
+  },
   plugins: [react()],
+  server: {
+    cors: true,
+  }
 })
