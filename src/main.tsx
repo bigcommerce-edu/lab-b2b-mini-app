a0389eb58aff679f1b97bb41c95e1ddcf5e17ec7
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-11-04 16:41:04 -0600

    Render PreviouslyOrdered component using DOM utils

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import useB2B, { type B2BUtils } from './hooks/useB2B';
 import HeaderLinks from './components/HeaderLinks';
+import PreviouslyOrdered from './components/PreviouslyOrdered';
 import { createRootElement } from './utils/dom';
 
 const createHeaderLinks = (b2bUtils: B2BUtils) => {
@@ -16,12 +17,19 @@ const createHeaderLinks = (b2bUtils: B2BUtils) => {
   }
 };
 
-// TODO: Implement createPreviouslyOrdered
-//  - Use DOM utils to create the root element
-//  - Use "previouslyOrdered" as the component ID
-//  - Pass b2bUtils to the PreviouslyOrdered component
+const createPreviouslyOrdered = (b2bUtils: B2BUtils) => {
+  const root = createRootElement('previouslyOrdered');
+
+  if (root) {
+    createRoot(root).render(
+      <StrictMode>
+        <PreviouslyOrdered b2bUtils={b2bUtils} />
+      </StrictMode>,
+    );
+  }
+};
 
 useB2B((b2bUtils) => {
   createHeaderLinks(b2bUtils);
-  // TODO: Call createPreviouslyOrdered
+  createPreviouslyOrdered(b2bUtils);
 });
