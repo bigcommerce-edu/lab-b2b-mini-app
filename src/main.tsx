965fd071866ee11233ff4e1dffdeeb61153781c7
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-11-17 16:19:01 -0600

    Implement useB2B hook to wait for window.b2b to be available

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import useB2B from './hooks/useB2B';
 
-// TODO: Remove this once useB2B hook is implemented
-  console.log('B2B custom app initialized');
+// TODO: Remove this once the components are implemented
+const TestApp = () => {
+  const b2b = useB2B();
+  return null;
+}
+
+const body = document.querySelector('body');
+if (body) {
+  const b2bRoot = document.createElement('div');
+  body.appendChild(b2bRoot);
+
+  createRoot(b2bRoot).render(
+    <StrictMode>
+      <TestApp />
+    </StrictMode>,
+  );
+}
