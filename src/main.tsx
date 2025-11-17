8c8f04417ff8cf821de3ca219d14775f1b7c9e00
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-11-17 15:41:48 -0600

    Render HeaderLinks component using DOM utils

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import useB2B from './hooks/useB2B';
+import HeaderLinks from './components/HeaderLinks';
+import { createRootElement } from './utils/dom';
 
-// TODO: Implement createHeaderLinks
-//  - Use DOM utils to create the root element
-//  - Use "headerLinks" as the component ID
+const createHeaderLinks = () => {
+  const root = createRootElement('headerLinks');
+
+  if (root) {
+    createRoot(root).render(
+      <StrictMode>
+        <HeaderLinks />
+      </StrictMode>,
+    );
+  }
+};
 
 // TODO: Implement createPreviouslyOrdered
 //  - Use DOM utils to create the root element
 //  - Use "previouslyOrdered" as the component ID
 
-// TODO: Remove this once the components are implemented
-const TestApp = () => {
-  const b2b = useB2B();
-  return null;
-}
-
-const body = document.querySelector('body');
-if (body) {
-  const b2bRoot = document.createElement('div');
-  body.appendChild(b2bRoot);
-
-  createRoot(b2bRoot).render(
-    <StrictMode>
-      <TestApp />
-    </StrictMode>,
-  );
-}
-
-// TODO: Call createHeaderLinks
+createHeaderLinks();
 // TODO: Call createPreviouslyOrdered
