28f09c1b63661b8db7bf214e4ad1120536f482da
Chris Nanninga <tarsusc81@gmail.com>
2026-05-07 14:11:07 -0500

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: './src/main.tsx',
+      output: {
+        // Remove hash from entry points (e.g., main.js)
+        entryFileNames: `assets/[name].js`,
+      }
+    },
+  },
+  plugins: [react(), cssInjectedByJsPlugin()],
+  server: {
+    cors: true,
+    hmr: false,
+  }
 })
