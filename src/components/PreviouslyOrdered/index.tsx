0e19bb7e3182b58718fa0816606e1b780a81bde9
Chris Nanninga <tarsusc81@gmail.com>
2026-06-25 16:04:08 -0500

    TODO: Use B2B GraphQL to fetch orderedProducts

--- src/components/PreviouslyOrdered/index.tsx
+++ src/components/PreviouslyOrdered/index.tsx
@@ -3,6 +3,11 @@ import useB2B from '../../hooks/useB2B';
 import { getPdpProductId } from '../../utils/dom';
 import './PreviouslyOrdered.css';
 
+// TODO: Define OrderedProductsQuery using GraphQL syntax
+
+// TODO: Define the schema for the OrderedProductsQuery response using zod
+//  - Also use the zod schema to define the OrderedProductsResponse type
+
 export default function PreviouslyOrdered() {
   const b2b = useB2B();
 
@@ -18,6 +23,11 @@ export default function PreviouslyOrdered() {
     // TODO: Remove this once the query is implemented
     setProductIsOrdered(true);
       
+    // TODO: Query to find out if product is in orderedProducts list
+    //  - Use the b2bFetch function to make the query
+    //  - Make sure to handle the result as a promise
+    //  - Parse the response using the OrderedProductsResponseSchema
+    //  - Set the productIsOrdered state based on whether data.orderedProducts.edges has any items
   }, [b2b]);
 
   return (
