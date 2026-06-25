42ba6c83460f083af269c39acc6a0be232bf7cbc
Chris Nanninga <tarsusc81@gmail.com>
2026-06-25 16:00:28 -0500

    TODO: Show 'previously ordered' notification on all PDPs

--- src/components/PreviouslyOrdered/index.tsx
+++ src/components/PreviouslyOrdered/index.tsx
@@ -3,10 +3,25 @@ import useB2B from '../../hooks/useB2B';
 import './PreviouslyOrdered.css';
 
 export default function PreviouslyOrdered() {
+  // TODO: Remove this once the component is implemented
   throw new Error('PreviouslyOrdered component not implemented');
+
+  // TODO: Get the B2B SDK with `useB2B`
+
+  // TODO: Set up React state to track whether the product is ordered
+
+  // TODO: Set up a side effect dependent on the B2B SDK
+    // TODO: Get product ID from specific DOM configuration
+    //  - getPdpProductId from DOM utils will do this
+
+    // TODO: The effect shouldn't do anything if the B2B SDK isn't available or the product ID is not found
+    
+    // TODO: Temporarily set productIsOrdered to true
+    
+
   return (
     <>
-      
+      {/* TODO: Implement JSX */}
     </>
   );
 }
